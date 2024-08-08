99d11d3adfcfe873f4f643ffb82e7b6d3fe69e41
Max Lythgoe <lythgoemax@gmail.com>
2024-08-08 14:06:41 -0600

    added first project

--- src/script.js
+++ src/script.js
@@ -47,73 +47,73 @@ window.onscroll = function() {
 
 
 // video blur overlay
-// class VideoWithBackground {
-//   video;
-//   canvas;
-//   step;
-//   ctx;
-//   posterImage;
+class VideoWithBackground {
+  video;
+  canvas;
+  step;
+  ctx;
+  posterImage;
 
-//   constructor(videoId, canvasId) {
-//     const mediaQuery = window.matchMedia("(prefers-reduced-motion: reduce)");
+  constructor(videoId, canvasId) {
+    const mediaQuery = window.matchMedia("(prefers-reduced-motion: reduce)");
 
-//     if (!mediaQuery.matches) {
-//       this.video = document.getElementById(videoId);
-//       this.canvas = document.getElementById(canvasId);
+    if (!mediaQuery.matches) {
+      this.video = document.getElementById(videoId);
+      this.canvas = document.getElementById(canvasId);
 
-//       window.addEventListener("load", this.init, false);
-//       window.addEventListener("unload", this.cleanup, false);
-//     }
-//   }
+      window.addEventListener("load", this.init, false);
+      window.addEventListener("unload", this.cleanup, false);
+    }
+  }
 
-//   draw = () => {
-//     this.ctx.drawImage(this.video, 0, 0, this.canvas.width, this.canvas.height);
-//   };
+  draw = () => {
+    this.ctx.drawImage(this.video, 0, 0, this.canvas.width, this.canvas.height);
+  };
 
-//   drawLoop = () => {
-//     this.draw();
-//     this.step = window.requestAnimationFrame(this.drawLoop);
-//   };
+  drawLoop = () => {
+    this.draw();
+    this.step = window.requestAnimationFrame(this.drawLoop);
+  };
 
-//   drawPause = () => {
-//     window.cancelAnimationFrame(this.step);
-//     this.step = undefined;
-//   };
+  drawPause = () => {
+    window.cancelAnimationFrame(this.step);
+    this.step = undefined;
+  };
 
-//   drawPoster = () => {
-//     if (this.posterImage.complete) {
-//       this.ctx.drawImage(this.posterImage, 0, 0, this.canvas.width, this.canvas.height);
-//     } else {
-//       this.posterImage.onload = () => {
-//         this.ctx.drawImage(this.posterImage, 0, 0, this.canvas.width, this.canvas.height);
-//       };
-//     }
-//   };
+  drawPoster = () => {
+    if (this.posterImage.complete) {
+      this.ctx.drawImage(this.posterImage, 0, 0, this.canvas.width, this.canvas.height);
+    } else {
+      this.posterImage.onload = () => {
+        this.ctx.drawImage(this.posterImage, 0, 0, this.canvas.width, this.canvas.height);
+      };
+    }
+  };
 
-//   init = () => {
-//     this.ctx = this.canvas.getContext("2d");
-//     this.ctx.filter = "blur(1px)";
+  init = () => {
+    this.ctx = this.canvas.getContext("2d");
+    this.ctx.filter = "blur(1px)";
 
-//     this.posterImage = new Image();
-//     this.posterImage.src = this.video.getAttribute("poster");
+    this.posterImage = new Image();
+    this.posterImage.src = this.video.getAttribute("poster");
 
-//     this.video.addEventListener("loadeddata", this.draw, false);
-//     this.video.addEventListener("seeked", this.draw, false);
-//     this.video.addEventListener("play", this.drawLoop, false);
-//     this.video.addEventListener("pause", this.drawPause, false);
-//     this.video.addEventListener("ended", this.drawPause, false);
+    this.video.addEventListener("loadeddata", this.draw, false);
+    this.video.addEventListener("seeked", this.draw, false);
+    this.video.addEventListener("play", this.drawLoop, false);
+    this.video.addEventListener("pause", this.drawPause, false);
+    this.video.addEventListener("ended", this.drawPause, false);
 
-//     // Draw the poster image if the video has not started playing yet
-//     this.drawPoster();
-//   };
+    // Draw the poster image if the video has not started playing yet
+    this.drawPoster();
+  };
 
-//   cleanup = () => {
-//     this.video.removeEventListener("loadeddata", this.draw);
-//     this.video.removeEventListener("seeked", this.draw);
-//     this.video.removeEventListener("play", this.drawLoop);
-//     this.video.removeEventListener("pause", this.drawPause);
-//     this.video.removeEventListener("ended", this.drawPause);
-//   };
-// }
+  cleanup = () => {
+    this.video.removeEventListener("loadeddata", this.draw);
+    this.video.removeEventListener("seeked", this.draw);
+    this.video.removeEventListener("play", this.drawLoop);
+    this.video.removeEventListener("pause", this.drawPause);
+    this.video.removeEventListener("ended", this.drawPause);
+  };
+}
 
-// const el = new VideoWithBackground("js-video", "js-canvas");
+const el = new VideoWithBackground("js-video", "js-canvas");
